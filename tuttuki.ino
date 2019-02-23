1127091a8df38bacb243f20903591f17ee5681d1
小野沢敦 <ats05@aol.jp>
2019-02-23 16:48:44 +0900

    障害物があった時にバックしてみる

--- tuttuki.ino
+++ tuttuki.ino
@@ -1,49 +1,86 @@
 #include <Servo.h>
 
+#define STATUS_FORWORD 100
+#define STATUS_BACK 200
+
 Servo servoL;
 Servo servoR;
+Servo servo3;
 int status = 100;
 int rotate = 0;
+const int distancePin = A0;
+const float Vcc = 3.5; 
 
 void setup() {
  
   Serial.begin(9600);
 
-  servoL.attach(3);
-  servoR.attach(2);
+  servoL.attach(2);
+  servoR.attach(3);
+//  servo3.attach(6);
+
+//  servoL.write(90);
+//  servoR.write(90);
 
   
+}
+
+/**
+ * pin ピン番号
+ * return センチメートル
+ */
+int readDistance(int pin){
+  float dist = Vcc * analogRead(pin) / 1023;  
+  dist = 26.549 * pow(dist, -1.2091); 
+
+  Serial.println(dist);
+
+  return dist;
 }
 
 void loop() {  
 
-  runServo(100, 0);
-//  switch(status){
-//    case 100:
-//      random();
-//      break;
-//  }
+  if(readDistance(distancePin) < 15 && readDistance(distancePin) > 5) {
+    if(status == STATUS_FORWORD) {
+      // 障害物があるとちょっと考え込む
+      runServo(0, 0);
+//      delay(2000);
+    }
+    status = STATUS_BACK;
+  }
+  else{
+    status = STATUS_FORWORD;
+  }
 
+
+  switch(status){
+    case STATUS_FORWORD:
+      runServo(10, 10);
+      break;
+    case STATUS_BACK:
+      runServo(-2, -8);
+      break;
+      
+  }
   
-//  delay(8000) ; //8秒間停止
-//  servo01.writeMicroseconds(544); //パルス幅値を代入：544~2400
-//  delay(8000) ; //8秒間停止
+  
 
 }
 
 /**
- * speed: integer 0~100
- * angle: integer 0~360
+ * speedR: integer -100~100
+ * speedL: integer -100~100
  */
-void runServo(int spd, int ang){
-
+void runServo(int speedR, int speedL){
   //TODO: アングルの実装
   
+  // 270  →90:180
+  
   // 停止:90 全力前進:0 に制御
-  servoR.write(90 - ((spd * 0.01) * 90));
+  servoR.write(90 - ((speedR * 0.01) * 90));
 
   // 停止:90 全力前進:180 に制御
-  servoL.write(90 + ((spd * 0.01) * 90));
+  servoL.write(90 + ((speedL * 0.01) * 90));
 
   
 }
